256172a55ae51724358ffacd48bd5b263cefb913
Vitor Paz <kngatuno@gmail.com>
2023-05-02 11:18:20 -0300

    alterando arquivo json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/viipaxx/js-developer-portfolio/main/data/profile.json'
     const fetcgin = await fetch(url)
     return await fetcgin.json()
 }
